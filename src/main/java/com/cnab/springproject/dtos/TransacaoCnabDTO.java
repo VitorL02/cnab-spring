0fdf105ed35ced416eed4fc9377b227d9cbfb568
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-12-26 10:31:53 -0300

    realizando ajustes no projeto antes de iniciar o processamento de CNAB por API REST

--- src/main/java/com/cnab/springproject/dtos/TransacaoCnabDTO.java
+++ src/main/java/com/cnab/springproject/dtos/TransacaoCnabDTO.java
@@ -1,6 +1,5 @@
-package com.cnab.springproject;
+package com.cnab.springproject.dtos;
 
 import java.math.BigDecimal;
 
-public record TransacaoCnabDTO(Integer tipo, String data, BigDecimal valor,Long cpf,String cartao,String hora,String donoDaLoja,String nomeDaLoja) {
-}
+public record TransacaoCnabDTO(Integer tipo, String data, BigDecimal valor,Long cpf,String cartao,String hora,String donoDaLoja,String nomeDaLoja) {}
--- src/main/java/com/cnab/springproject/entidades/TransacaoVO.java
+++ src/main/java/com/cnab/springproject/entidades/TransacaoVO.java
@@ -1,2 +1,40 @@
-package com.cnab.springproject.entidades;public class TransacaoVO {
+package com.cnab.springproject.entidades;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public record TransacaoVO(Long id, Integer tipo, Date data, BigDecimal valor, Long cpf, String cartao, Time hora, String donoDaLoja, String nomeDaLoja) {
+
+
+    public TransacaoVO withValor(BigDecimal valor){
+       return new TransacaoVO(this.id(),this.tipo(),this.data(),
+        valor,
+        this.cpf(),this.cartao(),this.hora(),
+        this.donoDaLoja(),this.nomeDaLoja());
+    }
+
+    public TransacaoVO withDate(String data) throws ParseException {
+        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+        var date = dateFormat.parse(data);
+
+        return new TransacaoVO(this.id(),this.tipo(),new Date (date.getTime()),
+                this.valor(),
+                this.cpf(),this.cartao(),this.hora(),
+                this.donoDaLoja(),this.nomeDaLoja());
+    }
+
+    public TransacaoVO withHora(String hora) throws ParseException {
+        var dateFormat = new SimpleDateFormat("HHmmss");
+        var hour = dateFormat.parse(hora);
+
+        return new TransacaoVO(this.id(),this.tipo(),this.data(),
+                this.valor(),
+                this.cpf(),this.cartao(),new Time (hour.getTime()),
+                this.donoDaLoja(),this.nomeDaLoja());
+    }
+
+
 }
