f178e3845073f226f8849905081a80c570069c4b
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-12-27 16:22:34 -0300

    Adicionando testes automatizados e a documentação do projeto e finalizando a parte do back end

--- src/main/java/com/cnab/springproject/controllers/CNABController.java
+++ src/main/java/com/cnab/springproject/controllers/CNABController.java
@@ -1,6 +1,8 @@
 package com.cnab.springproject.controllers;
 
 import com.cnab.springproject.services.CNABService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/cnab")
 public class CNABController {
-
+    private static final String CNAB = "CNAB - Processamento";
+    private static final String CNAB_RESUMO = "Esse endpoint permite o upload de um CNAB.txt";
+    private static final String CNAB_DESCRICAO = "Esse endpoint permite o upload de um CNAB em formato .txt para processamento assincrono dos dados do CNAB";
     @Autowired
     private CNABService cnabService;
 
-    @PostMapping("/upload")
-    public ResponseEntity upload(@RequestParam ("file") MultipartFile file) throws Exception {
+    @PostMapping(name = "/upload",consumes = {"multipart/form-data"})
+    @Tag(name=CNAB)
+    @Operation(summary = CNAB_RESUMO,description = CNAB_DESCRICAO)
+    public ResponseEntity upload(@RequestParam (value = "file") MultipartFile file) throws Exception {
 
         cnabService.uploadCnabFile(file);
         return ResponseEntity.status(HttpStatus.OK).body("Processamento iniciado. Aguarde!");
--- src/main/java/com/cnab/springproject/controllers/reports/TransacaoReportController.java
+++ src/main/java/com/cnab/springproject/controllers/reports/TransacaoReportController.java
@@ -2,6 +2,8 @@ package com.cnab.springproject.controllers.reports;
 
 import com.cnab.springproject.entidades.TransacaoReport;
 import com.cnab.springproject.services.reports.TransacaoReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,15 @@ import java.util.List;
 @RequestMapping("report")
 public class TransacaoReportController {
 
+    private static final String REPORTS = "Relatorios - Processamento";
+    private static final String REPORTS_RESUMO = "Esse endpoint fornece um relatorio de CNABS processados por loja";
+    private static final String REPORTS_DESCRICAO = "Esse endpoint lista os relatorios de CNAB processados de acordo com o nome da loja, agrupando e informando total de saldo da loja juntamente com as transações realizadas";
     @Autowired
     private TransacaoReportService transacaoReportService;
 
     @GetMapping
+    @Tag(name=REPORTS)
+    @Operation(summary = REPORTS_RESUMO,description = REPORTS_DESCRICAO)
     List<TransacaoReport> listAll(){
         List<TransacaoReport> transacaoReports = transacaoReportService.listTransactionsNameStore();
         return transacaoReports;
