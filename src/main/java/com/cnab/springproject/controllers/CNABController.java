1114e2efdb5c36c1dc2a408f48082f3c89437b57
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-12-28 16:33:53 -0300

    corrigindo cors e endpoint de upload

--- src/main/java/com/cnab/springproject/controllers/CNABController.java
+++ src/main/java/com/cnab/springproject/controllers/CNABController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -21,8 +18,7 @@ public class CNABController {
     @Autowired
     private CNABService cnabService;
 
-    @PostMapping(name = "/upload",consumes = {"multipart/form-data"})
-    @Tag(name=CNAB)
+    @PostMapping(path = "/upload",consumes = {"multipart/form-data"})
     @Operation(summary = CNAB_RESUMO,description = CNAB_DESCRICAO)
     public ResponseEntity upload(@RequestParam (value = "file") MultipartFile file) throws Exception {
 
--- src/main/java/com/cnab/springproject/controllers/reports/TransacaoReportController.java
+++ src/main/java/com/cnab/springproject/controllers/reports/TransacaoReportController.java
@@ -5,6 +5,7 @@ import com.cnab.springproject.services.reports.TransacaoReportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
